059058bcf0f1f2ee998aa00464dd4c96b4773c9b
Sai7xp <saisumanth2303@gmail.com>
2024-08-26 22:27:31 +0530

    use case of new() with examples

--- basics/08Pointers/new_vs_make.go
+++ basics/08Pointers/new_vs_make.go
@@ -10,7 +10,7 @@ import "fmt"
 func newVsMake() {
 	fmt.Println("---------------- new vs make ---------------")
 	/*
-		what does make() do?
+		🌻 what does make() do?
 		It allocates & initialize memory for slices, maps, and channels only.
 		Unlike new, make does not return a pointer.
 
@@ -28,6 +28,29 @@ func newVsMake() {
 		channel <- 9
 	}()
 	fmt.Println(<-channel)
+
+	/*
+		🌻 what does new() do?
+		Allocates the memory to a variable and returns pointer to that memory.
+		It does not initialize the memory beyond zeroing it, so the allocated memory is zero-valued.
+		new is used to allocate memory for any type, including custom types
+
+		p := new(T)  // T can be any type
+
+		This is commonly used when working with user-defined types (e.g., structs),
+		when we want to create an instance but don’t want to initialize it with specific values.
+	*/
+
+	p := new(int)
+	fmt.Println("p created using new(): ", p)
+
+	type Person struct {
+		Name string
+		Age  int
+	}
+
+	per := new(Person) // p is of type *Person, with Name = "" and Age = 0
+	fmt.Printf("default person : %#v\n", per)
 
 	fmt.Println("---------------- new vs make ---------------")
 }
