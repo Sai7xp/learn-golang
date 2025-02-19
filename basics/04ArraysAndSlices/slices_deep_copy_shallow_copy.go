2126f41619ffa28b1fd8b0ae5be34dfa2f4f45cd
Sai7xp <saisumanth2303@gmail.com>
2025-02-19 20:05:22 +0530

    creating clone of slice using copy() and append() methods - deep copy

--- basics/04ArraysAndSlices/slices_deep_copy_shallow_copy.go
+++ basics/04ArraysAndSlices/slices_deep_copy_shallow_copy.go
@@ -4,7 +4,9 @@
  */
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func slicesDeepCopyShallowCopy() {
 	fmt.Println("Slice Shallow Copy and Deep Copy")
@@ -34,4 +36,18 @@ func slicesDeepCopyShallowCopy() {
 	copySlice[1] = 999
 	fmt.Println("orgSlice :", orgSlice)   // [0 0 876 0 0] 😌 original slice is safe
 	fmt.Println("copySlice :", copySlice) // [0 999 876 0 0]
+
+	/*
+		Copying/Cloning slice using append method
+	*/
+	nums := []int{1, 2, 3, 4, 5}
+
+	// copy slice using
+	duplicateSlice := append(make([]int, 0), nums...)
+
+	// modify the original slice
+	nums[0] = 100
+	fmt.Println("Original Slice: ", nums)            // [100, 2, 3, 4, 5]
+	fmt.Println("Duplicate Slice: ", duplicateSlice) // [1, 2, 3, 4, 5] (remains unchanged)
+
 }
