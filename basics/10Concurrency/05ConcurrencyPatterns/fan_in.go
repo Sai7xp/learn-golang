d8d28cdf49fc1796709dddae234cd702ca2ad23c
Sai7xp <saisumanth2303@gmail.com>
2025-05-21 18:15:48 +0530

    Fan In - Variadic fn

--- basics/10Concurrency/05ConcurrencyPatterns/fan_in.go
+++ basics/10Concurrency/05ConcurrencyPatterns/fan_in.go
@@ -43,9 +43,16 @@ InfiniteLoop:
 		}
 	}
 
+	fmt.Println(">>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<")
+
+	stream1 := produceEvenNums()
+	stream2 := produceOddNums()
+	stream3 := produceOddNums()
+
 	/// 🌻 BUT, is there any better way of doing this ? YES, FAN IN Pattern
-	multiplexedChan := fanIn(evenC, oddC)
-	for msg := range multiplexedChan {
+	// output := fanIn(stream1, stream2)
+	output := fanInPro(stream1, stream2, stream3)
+	for msg := range output {
 		fmt.Println(msg)
 	}
 
@@ -80,11 +87,33 @@ func fanIn(c1 <-chan int, c2 <-chan int) <-chan int {
 	return multiplexedStream
 }
 
+func fanInPro(inputChannels ...<-chan int) <-chan int {
+	multiplexedStream := make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(len(inputChannels))
+
+	for _, eachInput := range inputChannels {
+		go func(c <-chan int) {
+			defer wg.Done()
+			for data := range c {
+				multiplexedStream <- data
+			}
+		}(eachInput)
+	}
+
+	go func() {
+		wg.Wait() // wait for both to complete and close the multiplexedStream
+		close(multiplexedStream)
+	}()
+
+	return multiplexedStream
+}
+
 func produceEvenNums() <-chan int {
 	c := make(chan int)
 	go func() {
 		defer close(c)
-		for even := 0; even <= 10; even += 2 {
+		for even := 2; even <= 10; even += 2 {
 			c <- even
 			time.Sleep(time.Second * 1)
 		}
